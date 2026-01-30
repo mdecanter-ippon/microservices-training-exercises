ae575c98c0a79a0d95aa94f96964592328e2458a
Halim IZZOUZI <hizzouzi@ippon.fr>
2026-01-30 16:12:08 +0100

    Remove @SecurityScheme from OpenApiConfig (exercise added in Step 5 instructions)

--- shipment-service/src/main/java/com/dornach/shipment/config/OpenApiConfig.java
+++ shipment-service/src/main/java/com/dornach/shipment/config/OpenApiConfig.java
@@ -1,12 +1,9 @@
 package com.dornach.shipment.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
@@ -35,14 +32,7 @@ import org.springframework.context.annotation.Configuration;
         servers = {
                 @Server(url = "http://localhost:8082", description = "Local Development"),
                 @Server(url = "https://api.dornach.com", description = "Production")
-        },
-        security = @SecurityRequirement(name = "bearerAuth")
-)
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
+        }
 )
 public class OpenApiConfig {
 }
